5dddc573db26fa2e1804b82601f134be8e52009c
juliano <mozerlogistica@gmail.com>
2024-10-08 12:18:59 -0300

    configuando o uglify e reponsividade

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 // Função para compilar os arquivos SCSS
 function styles() {
@@ -16,9 +23,10 @@ function images() {
 }
 
 // Exporta a função de estilos como tarefa padrão
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 // Tarefa que observa as mudanças nos arquivos SCSS
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', styles); // Monitora as alterações e executa 'styles'
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) 
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts)) // Monitora as alterações e executa 'styles'
 }
